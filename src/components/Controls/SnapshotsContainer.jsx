62527c2be7c03513e7fc87177642dff1dba944cb
dcorn068 <dcorn068@users.noreply.github.com>
2018-10-01 15:58:04 -0400

    add menu to snapshots: apply / copy link

--- src/components/Controls/SnapshotsContainer.jsx
+++ src/components/Controls/SnapshotsContainer.jsx
@@ -4,6 +4,8 @@ import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import PlayArrowIcon from '@material-ui/icons/PlayArrowRounded';
+import ShareIcon from '@material-ui/icons/Share';
 
 const Container = styled.div`
   display: grid;
@@ -22,19 +24,71 @@ const Container = styled.div`
 `;
 
 export default class SnapshotsContainer extends Component {
+  state = {
+    anchorEl: null
+  };
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
+    const { anchorEl } = this.state;
+
     return (
       <Container>
         {this.props.snapshots.map(ss => {
           return (
-            <Button
-              className={`snapshotButton snapshotButton_${ss.id}`}
-              key={JSON.stringify(ss)}
-              onClick={() => this.props.onShapshotClick(ss.id)}
-            >
-              <img src={ss.image} alt={`snapshot_${ss.id}`} />
-              <Typography variant="caption">Snapshot #{ss.id + 1}</Typography>
-            </Button>
+            <React.Fragment key={JSON.stringify(ss)}>
+              <Button
+                aria-owns={anchorEl ? 'simple-menu' : null}
+                aria-haspopup="true"
+                className={`snapshotButton snapshotButton_${ss.id}`}
+                onClick={this.handleClick}
+              >
+                <img src={ss.image} alt={`snapshot_${ss.id}`} />
+                <Typography variant="caption">Snapshot #{ss.id + 1}</Typography>
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleClose}
+              >
+                <MenuItem
+                  style={{ display: 'grid', gridTemplateColumns: '1fr auto' }}
+                  onClick={() => {
+                    this.props.onShapshotClick(ss.id);
+                    this.handleClose();
+                  }}
+                >
+                  Apply Snapshot{' '}
+                  <PlayArrowIcon
+                    style={{
+                      marginLeft: 10,
+                      justifySelf: 'right',
+                      color: 'steelblue'
+                    }}
+                  />
+                </MenuItem>
+                <MenuItem
+                  style={{ display: 'grid', gridTemplateColumns: '1fr auto' }}
+                  onClick={this.handleClose}
+                >
+                  Copy Link to Snapshot{' '}
+                  <ShareIcon
+                    style={{
+                      marginLeft: 10,
+                      justifySelf: 'right',
+                      color: 'steelblue'
+                    }}
+                  />
+                </MenuItem>
+              </Menu>
+            </React.Fragment>
           );
         })}
       </Container>
