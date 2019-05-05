d4acc1d6e387c6ae42b788977c056b5c51cd73b1
Daniel Corner <dcorn068@gmail.com>
2019-05-04 23:02:18 -0400

    add AppTitle and change tindex title to Goodjob

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React, { Component } from 'react';
 import './App.css';
 import AppLayout from './components/AppLayout';
 import ContextProvider from './components/Context/ContextProvider';
+import styled from 'styled-components';
 
 const theme = createMuiTheme({
   palette: {
@@ -18,12 +19,28 @@ const theme = createMuiTheme({
   },
 });
 
+const AppTitleStyles = styled.div`
+font-family: system-ui;
+  h1{
+  font-size: 24px;
+  margin: 0 12px;
+  }
+span{
+  font-size: 18px;
+  text-shadow: none;
+}
+`
+
+const AppTitle = () => <AppTitleStyles>
+  <h1>Goodjob 🎈 <span>Explore Canadian Careers</span></h1>
+</AppTitleStyles>
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <ContextProvider>
           {/* <Navbar /> */}
+          <AppTitle />
           <Router>
             <AppLayout path="/" />
             <AppLayout path="/:filtersQuery" />
