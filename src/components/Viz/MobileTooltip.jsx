476fc6e100da1d5b9c994f23fdbae15c889b38c3
dcorn068 <dcorn068@users.noreply.github.com>
2019-05-23 00:31:23 -0400

    transform mobile tooltip into pictogram

--- src/components/Viz/MobileTooltip.jsx
+++ src/components/Viz/MobileTooltip.jsx
@@ -1,12 +1,13 @@
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
-import MoneyIcon from '@material-ui/icons/MonetizationOnOutlined';
-import SchoolIcon from '@material-ui/icons/SchoolRounded';
-import WarningIcon from '@material-ui/icons/WarningRounded';
 import OpenInNewIcon from '@material-ui/icons/OpenInNewRounded';
 import React from 'react';
 import styled from 'styled-components';
 import { TABLET_MIN_WIDTH } from '../../utils/constants';
+
+const ICON_WIDTH = 32;
+const ICON_SCALE = 1.5;
+const ICON_DY = -2;
 
 const getMobileTooltipStyles = ({
   salaryMedPercent,
@@ -23,6 +24,7 @@ const getMobileTooltipStyles = ({
   }
   .btnMoreInfo {
     padding: 5px 12px;
+    text-transform: none;
     span:first-child {
       display: grid;
       grid-template-columns: 1fr auto;
@@ -51,19 +53,17 @@ const getMobileTooltipStyles = ({
     font-weight: bold;
     display: grid;
     grid-gap: 2px;
-    place-items: center start;
-    width: 250px;
-
+    place-items: center flex-end;
   }
   .industry {
     font-style: italic;
     grid-column: 1 / -1;
-    font-size: 0.9em;
   }
   .grid {
+    position: relative;
     display: grid;
     align-items: start;
-    grid-template-columns: auto 1fr;
+    grid-template-columns: 72px 1fr;
     grid-gap: 15px 30px;
     margin-bottom: 18px;
     @media (min-width: ${TABLET_MIN_WIDTH}px) {
@@ -71,6 +71,7 @@ const getMobileTooltipStyles = ({
     }
   }
   .data {
+    font-size: 0.8em;
     margin-top: 3px;
   }
   .center {
@@ -83,14 +84,11 @@ const getMobileTooltipStyles = ({
     text-align: left;
   }
   .iconTitle {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    grid-gap: 4px;
-    place-items: center start;
+    font-weight: bold;
   }
   .bar {
+    height: ${ICON_WIDTH*1}px;
     align-self: center;
-    height: 25px;
     &.salaryBar {
       background: lime;
       width: ${salaryMedPercent * 100}%;
@@ -106,20 +104,6 @@ const getMobileTooltipStyles = ({
       width: ${educationPercent * 100}%;
       clip-path: url(#studyClip);
     }
-    &:not(.emptyBar) {
-      margin: -1px 0 0 -1px;
-      position: relative;
-    }
-    &.emptyBar {
-      position: absolute;
-      left: 1px;
-      right: 1px;
-      top: 0;
-      bottom: 0;
-      height: calc(100% - 1px);
-      box-sizing: border-box;
-      background: white;
-    }
   }
   .salary {
     margin-left: -1ch;
@@ -127,8 +111,18 @@ const getMobileTooltipStyles = ({
   .study {
     margin-left: -1.5ch;
   }
+  .extraText{
+    position: absolute;
+    left: calc(5px + 1ch);
+    &.extraText1{
+    bottom: -14px;
+    }
+    &.extraText2{
+    bottom: -28px;
+    }
+  }
 `;
-const ICON_WIDTH = 22
+
 
 const MobileTooltipContents = ({ data }) => {
   const { job, industry, salaryMed, color, automationRisk, yearsStudy } = data;
@@ -147,72 +141,121 @@ const MobileTooltipContents = ({ data }) => {
     <React.Fragment>
       <svg width="0" height="0">
         <defs>
-          <clipPath id="moneyClip" >
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-            <path d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z" />
-            <path style={{transform: `translateX(${ICON_WIDTH}px)`}} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-            <path style={{transform: `translateX(${ICON_WIDTH}px)`}} d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z" />
-            <path style={{transform: `translateX(${ICON_WIDTH*2}px)`}} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-            <path style={{transform: `translateX(${ICON_WIDTH*2}px)`}} d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z" />
-            <path style={{transform: `translateX(${ICON_WIDTH*3}px)`}} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-            <path style={{transform: `translateX(${ICON_WIDTH*3}px)`}} d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z" />
-            <path style={{transform: `translateX(${ICON_WIDTH*4}px)`}} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-            <path style={{transform: `translateX(${ICON_WIDTH*4}px)`}} d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z" />
+          <clipPath id="moneyClip">
+            <path style={{ transform: `translate(${ICON_WIDTH*0}px, ${ICON_DY}px) scale(${ICON_SCALE})` }} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+            <path style={{ transform: `translate(${ICON_WIDTH*0}px, ${ICON_DY}px) scale(${ICON_SCALE})` }} d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z" />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH*1}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH*1}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 2}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 2}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 3}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 3}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 4}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 4}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M12.31 11.14c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z"
+            />
           </clipPath>
-          <clipPath id="riskClip" >
-            <path d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH}px)`}} d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH*2}px)`}} d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH*3}px)`}} d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH*4}px)`}} d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"></path>
+          <clipPath id="riskClip">
+            <path style={{ transform: `translate(${ICON_WIDTH*0}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+            d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z" />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH*1}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 2}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 3}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 4}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M4.47 21h15.06c1.54 0 2.5-1.67 1.73-3L13.73 4.99c-.77-1.33-2.69-1.33-3.46 0L2.74 18c-.77 1.33.19 3 1.73 3zM12 14c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"
+            />
           </clipPath>
-          <clipPath id="studyClip" >
-            <path d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH}px)`}} d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH*2}px)`}} d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH*3}px)`}} d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"></path>
-            <path style={{transform: `translateX(${ICON_WIDTH*4}px)`}} d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"></path>
+          <clipPath id="studyClip">
+            <path style={{ transform: `translate(${ICON_WIDTH*0}px, ${ICON_DY}px) scale(${ICON_SCALE})` }} d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z" />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH*1}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 2}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 3}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"
+            />
+            <path
+              style={{ transform: `translate(${ICON_WIDTH * 4}px, ${ICON_DY}px) scale(${ICON_SCALE})` }}
+              d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 0 0-1.92 0z"
+            />
           </clipPath>
         </defs>
       </svg>
       <MobileTooltipStyles>
         <h3 className="title textAlignLeft">{job}</h3>
+
         <div className="grid">
           <div className="data industry textAlignLeft">{industry}</div>
 
           <div className="heading">
             <div className="iconTitle">Salary:</div>
-            <div className="bar salaryBar">
+            <div className="data textAlignLeft salary">
+              <strong>${salaryMed.toFixed(0)}K</strong> per year
             </div>
-          </div>
-          <div className="data textAlignLeft salary">
-            <strong>${salaryMed.toFixed(0)}K</strong> per year
           </div>
 
+          <div className="bar salaryBar" />
+
           <div className="heading">
-            <div className="iconTitle">
-              Study:
-            </div>
-            <div className="bar educationBar">
-            </div>
-          </div>
-          <div>
+            <div className="iconTitle">Study:</div>
             <div className="data textAlignLeft study">
               ~ <strong>{yearsStudy.toFixed(1)} years</strong>
             </div>
           </div>
 
+          <div className="bar educationBar" />
+
           <div className="heading">
-            <div className="iconTitle">
-              Risk:
-            </div>
-            <div className="bar riskBar">
+            <div className="iconTitle">Risk:</div>
+            <div className="data textAlignLeft automation">
+              <strong>{(automationRisk * 100).toFixed(0)}%</strong> chance
             </div>
           </div>
-          <div className="data textAlignLeft automation">
-            <strong>{(automationRisk * 100).toFixed(0)}%</strong> chance of
-            tasks being replaced by machines
-          </div>
+
+          <div className="bar riskBar" />
+            <div className="data textAlignLeft extraText extraText1">
+              of tasks being
+            </div>
+            <div className="data textAlignLeft extraText extraText2">
+              replaced by machines
+            </div>
         </div>
         <div className="btnMoreInfoWrapper">
           <Button variant="outlined" className="btnMoreInfo" disabled={true}>
