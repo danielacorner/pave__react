c63d2bb880da2a4ff92e46b2fc66dd34d24b3222
dcorn068 <dcorn068@users.noreply.github.com>
2019-05-27 21:57:34 -0400

    add service worker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 // import Navbar from './components/Navbar';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ContextProvider from './components/Context/ContextProvider';
 import AppWithContext from './AppWithContext';
+import { Button } from '@material-ui/core';
 // import styled, { keyframes } from 'styled-components';
 
 const theme = createMuiTheme({
@@ -18,11 +19,43 @@ const theme = createMuiTheme({
   },
 });
 
+const AddToHomeScreenBanner = ({ deferredPrompt, setDeferredPrompt }) => (
+  <Button
+    onClick={() => {
+      deferredPrompt.prompt();
+      deferredPrompt.userChocie.then(choiceResult => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        setDeferredPrompt(null);
+      });
+    }}
+  >
+    Hello!
+  </Button>
+);
+
 function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  window.addEventListener('beforeinstallprompt', e => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    setDeferredPrompt(e);
+  });
   return (
     <MuiThemeProvider theme={theme}>
       <ContextProvider>
         <AppWithContext />
+        {deferredPrompt && (
+          <AddToHomeScreenBanner
+            deferredPrompt={deferredPrompt}
+            setDeferredPrompt={setDeferredPrompt}
+          />
+        )}
       </ContextProvider>
     </MuiThemeProvider>
   );
