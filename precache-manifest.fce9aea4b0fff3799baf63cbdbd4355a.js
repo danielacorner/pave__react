3d9aa4ea5d2dcd71568e67f64ac52fd13f39768f
dcorn068 <dcorn068@users.noreply.github.com>
2019-06-04 00:40:24 -0400

    Updates

--- precache-manifest.fce9aea4b0fff3799baf63cbdbd4355a.js
+++ precache-manifest.fce9aea4b0fff3799baf63cbdbd4355a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "feea655b928c821d0c0fc0da31a23895",
+    "revision": "975aa3ffd0f5f5c5b760f71697398b2c",
     "url": "/pave__react/index.html"
   },
   {
-    "revision": "3cc3f67ad0c2b049f82e",
+    "revision": "a809785e7b48bd097db1",
     "url": "/pave__react/static/css/main.563a2a4b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pave__react/static/js/2.06acb202.chunk.js"
   },
   {
-    "revision": "3cc3f67ad0c2b049f82e",
-    "url": "/pave__react/static/js/main.4df2f8d3.chunk.js"
+    "revision": "a809785e7b48bd097db1",
+    "url": "/pave__react/static/js/main.44c9402c.chunk.js"
   },
   {
     "revision": "6c0dfc4ea0df5bb80d3c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pave__react/precache-manifest.a901fbccf7bc93b877148dca58f05ac6.js"
+  "/pave__react/precache-manifest.fce9aea4b0fff3799baf63cbdbd4355a.js"
 );
 
 self.addEventListener('message', (event) => {
